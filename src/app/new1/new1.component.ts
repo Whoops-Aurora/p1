ccb30736cafa5104c104f6bd62dcc30841295d8c
frosty <adwaitsadalage@gmail.com>
2023-11-29 11:19:05 +0530

    " Usage of looping and printing the arrays in FOR loop"

--- src/app/new1/new1.component.ts
+++ src/app/new1/new1.component.ts
@@ -9,4 +9,10 @@ export class New1Component {
   fname: string = 'John';
   lname: string = 'Doe';
   age: number = 30;
+  city:Array<string> = ['Mumbai', 'Pune', 'Delhi'];
+  constructor() { 
+    console.log('New1Component created');
+    this.city.push('Chennai');
+    console.log(this.city+'chennai pushed to city array');
+  }
 }
