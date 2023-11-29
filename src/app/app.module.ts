c29e747cf019b9395d7fb2b0b72a612183740d59
frosty <adwaitsadalage@gmail.com>
2023-11-29 10:37:45 +0530

    "made changed to bootstarp changeing from appComponent to new1"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { New1Component } from './new1/new1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    New1Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [New1Component]
 })
 export class AppModule { }
---
+++ src/app/new1/new1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new1',
+  templateUrl: './new1.component.html',
+  styleUrl: './new1.component.css'
+})
+export class New1Component {
+
+}
