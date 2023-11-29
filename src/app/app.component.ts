f29b7c50a17cb007c0fabb0dfb2d049d7902af36
frosty <adwaitsadalage@gmail.com>
2023-11-29 10:19:00 +0530

    "added table and onClick function in app.compenent.ts within class and used .html to use it with button name as 'add'"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KLETECH';
+  subtitle = 'Welcome to KLETECH';
+  id=1;
+  Name='Habibi';
+  age=20;
+  id1=2;
+  Name1='Habibi1';
+  age1=21;
+
+  a:number=10;
+  b:number=20;
+  c:number;
+  constructor(){
+    console.log('Constructor is called');
+    console.log("a");
+    this.c=this.a+this.b;
+  }
+  add(){
+    this.c=this.a+this.b+this.c;
+  }
 }
