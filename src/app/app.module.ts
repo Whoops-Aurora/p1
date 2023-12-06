d9d489e6b5e92bb827c3125f07712a95bba81156
Adwait S <67193262+Whoops-Aurora@users.noreply.github.com>
2023-12-06 10:32:30 +0530

    "Completed all modules  remaining: images sec"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here (for two
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { New1Component } from './new1/new1.component';
+import { New2Component } from './new2/new2.component';
+import { DisplayService } from './display.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    New1Component
+    New1Component,
+    New2Component
   ],
   imports: [
     BrowserModule,
@@ -17,7 +20,7 @@ import { New1Component } from './new1/new1.component';
     FormsModule // <-- import the FormsModule before binding with [(ngModel)]
     
   ],
-  providers: [],
-  bootstrap: [New1Component]
+  providers: [DisplayService],
+  bootstrap: [New2Component]
 })
 export class AppModule { }
